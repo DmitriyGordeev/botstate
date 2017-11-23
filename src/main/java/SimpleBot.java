ced0463dfe503418679fba3f31e6b71a299cd44f
Weekend Master <weekend@super.com>
2017-11-23 13:34:36 +0300

    init repositoiry

---
+++ src/main/java/SimpleBot.java
@@ -0,0 +1,38 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class SimpleBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "dimas_test_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "415598199:AAHFEgk3tjCeQmZ7YDBmH90XC0PYN0PyuSw";
+    }
+}
